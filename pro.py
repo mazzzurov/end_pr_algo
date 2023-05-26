782a3924a9422bb46d303f5f0f3c4ced822cefcf
mazzzurov <77230471+mazzzurov@users.noreply.github.com>
2023-05-26 23:48:07 +0300

    Update pro.py

--- pro.py
+++ pro.py
@@ -1,7 +1,10 @@
 import sys
  
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
-from PyQt5.QtWidgets import QLabel, QLineEdit
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+
+
  
  
 class Calculator(QWidget):
@@ -10,100 +13,101 @@ class Calculator(QWidget):
         self.initUI()
         self.ans = 0
         self.ans2 = 0
-        self.a = 10
         self.t = ""
+        
  
     def initUI(self):
-        self.setGeometry(300, 300, 225, 370)
-        self.setWindowTitle('калькулятор')
+        self.setGeometry(300, 300, 450, 380)
+        self.setWindowTitle('Simple Calculator')
  
         self.label = QLabel(self)
         self.label.setText("0")
         self.label.resize(225, 95)
-        self.label.move(0, 0)
+        self.label.move(10, 0)
         
         # первая строчка в калькуляторе
         self.num_1 = QPushButton('1', self)
-        self.num_1.resize(50, 50)
+        self.num_1.resize(90, 50)
         self.num_1.move(5, 100)
         self.num_1.clicked.connect(self.res)
         
+        
         self.num_2 = QPushButton('2', self)
-        self.num_2.resize(50, 50)
-        self.num_2.move(60, 100)
+        self.num_2.resize(90, 50)
+        self.num_2.move(100, 100)
         self.num_2.clicked.connect(self.res)
  
         self.num_3 = QPushButton('3', self)
-        self.num_3.resize(50, 50)
-        self.num_3.move(115, 100)
+        self.num_3.resize(90, 50)
+        self.num_3.move(200, 100)
         self.num_3.clicked.connect(self.res)
         
         self.num_div = QPushButton('/', self)
-        self.num_div.resize(50, 50)
-        self.num_div.move(170, 100)
+        self.num_div.resize(90, 50)
+        self.num_div.move(300, 100)
         self.num_div.clicked.connect(self.div)
  
          # вторая строчка в калькуляторе
         self.num_4 = QPushButton('4', self)
-        self.num_4.resize(50, 50)
+        self.num_4.resize(90, 50)
         self.num_4.move(5, 155)
         self.num_4.clicked.connect(self.res)
         
         self.num_5 = QPushButton('5', self)
-        self.num_5.resize(50, 50)
-        self.num_5.move(60, 155)
+        self.num_5.resize(90, 50)
+        self.num_5.move(100, 155)
         self.num_5.clicked.connect(self.res)
  
         self.num_6 = QPushButton('6', self)
-        self.num_6.resize(50, 50)
-        self.num_6.move(115, 155)
+        self.num_6.resize(90, 50)
+        self.num_6.move(200, 155)
         self.num_6.clicked.connect(self.res)
         
         self.num_mul = QPushButton('*', self)
-        self.num_mul.resize(50, 50)
-        self.num_mul.move(170, 155)
+        self.num_mul.resize(90, 50)
+        self.num_mul.move(300, 155)
         self.num_mul.clicked.connect(self.mul)
  
         # третья строчка в калькуляторе
         self.num_7 = QPushButton('7', self)
-        self.num_7.resize(50, 50)
+        self.num_7.resize(90, 50)
         self.num_7.move(5, 210)
         self.num_7.clicked.connect(self.res)
         
         self.num_8 = QPushButton('8', self)
-        self.num_8.resize(50, 50)
-        self.num_8.move(60, 210)
+        self.num_8.resize(90, 50)
+        self.num_8.move(100, 210)
         self.num_8.clicked.connect(self.res)
  
         self.num_9 = QPushButton('9', self)
-        self.num_9.resize(50, 50)
-        self.num_9.move(115, 210)
+        self.num_9.resize(90, 50)
+        self.num_9.move(200, 210)
         self.num_9.clicked.connect(self.res)
         
         self.num_plus = QPushButton('+', self)
-        self.num_plus.resize(50, 50)
-        self.num_plus.move(170, 210)
+        self.num_plus.resize(90, 50)
+        self.num_plus.move(300, 210)
         self.num_plus.clicked.connect(self.add)
  
         # четвёртая строчка в калькуляторе
         self.num_C = QPushButton('C', self)
-        self.num_C.resize(50, 50)
+        self.num_C.resize(90, 50)
         self.num_C.move(5, 265)
         self.num_C.clicked.connect(self.C)
         
         self.num_0 = QPushButton('0', self)
-        self.num_0.resize(50, 50)
-        self.num_0.move(60, 265)
+        self.num_0.resize(90, 50)
+        self.num_0.move(100, 265)
         self.num_0.clicked.connect(self.res)
  
         self.num_CE = QPushButton('CE', self)
-        self.num_CE.resize(50, 50)
-        self.num_CE.move(115, 265)
+        self.num_CE.resize(90, 50)
+        self.num_CE.move(200, 265)
         self.num_CE.clicked.connect(self.C)
         
         self.num_minus = QPushButton('-', self)
-        self.num_minus.resize(50, 50)
-        self.num_minus.move(170, 265)
+        self.num_minus.resize(90, 50)
+        self.num_minus.move(300, 265)
         self.num_minus.clicked.connect(self.minus)
  
         # пятая строчка в калькуляторе
@@ -113,20 +117,25 @@ class Calculator(QWidget):
         #self.num_dot.clicked.connect(self.dot)
         
         self.num_p_m = QPushButton('±', self)
-        self.num_p_m.resize(50, 50)
-        self.num_p_m.move(60, 320)
+        self.num_p_m.resize(190, 50)
+        self.num_p_m.move(5, 320)
         self.num_p_m.clicked.connect(self.p_m)
  
         self.num_eq = QPushButton('=', self)
-        self.num_eq.resize(105, 50)
-        self.num_eq.move(115, 320)
+        self.num_eq.resize(190, 50)
+        self.num_eq.move(205, 320)
         self.num_eq.clicked.connect(self.eq)
+
+        self.stepen = QPushButton('^', self)
+        self.stepen.resize(40, 270)
+        self.stepen.move(405, 100)
+        self.stepen.clicked.connect(self.step)
         
     def eq(self):
         if self.t == "+":
-                self.label.setText(str(self.ans + self.ans2))
-                self.ans = 0
-                self.ans2 = 0
+            self.label.setText(str(self.ans + self.ans2))
+            self.ans = 0
+            self.ans2 = 0
         elif self.t == "-":
             self.label.setText(str(self.ans - self.ans2))
             self.ans = 0
@@ -139,6 +148,10 @@ class Calculator(QWidget):
             self.label.setText(str(self.ans / self.ans2))
             self.ans = 0
             self.ans2 = 0
+        elif self.t == '^':
+            self.label.setText(str(self.ans ** self.ans2))
+            self.ans = 0
+            self.ans2 = 0
         else:
             self.label.setText(str(self.ans))
         
@@ -149,6 +162,10 @@ class Calculator(QWidget):
         else:
             self.ans *= -1
             self.label.setText(str(self.ans))
+
+    def step(self):
+        self.t = "^"
+        self.label.setText(str(self.ans))
         
     def C(self):
         self.t = ''
@@ -182,10 +199,18 @@ class Calculator(QWidget):
         self.t = "/"
         self.label.setText(str(self.ans))
  
- 
+
  
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Calculator()
+    
+    pal = ex.palette()
+    pal.setColor(QPalette.Normal, QPalette.Window, QColor("#FF6600"))
+    pal.setColor(QPalette.Inactive, QPalette.Window, QColor("#ff0000"))
+    ex.setPalette(pal)
+    
+
+
     ex.show()
     sys.exit(app.exec())
